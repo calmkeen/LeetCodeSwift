4efc53af12ed6b6ae1eb0059f737eca36e9e3ad3
Younsung <78361650+calmkeen@users.noreply.github.com>
2024-04-17 09:11:26 +0900

    Time: 46 ms (83.01%), Space: 16.4 MB (13.73%) - LeetHub

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.swift
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.swift
@@ -1,9 +1,5 @@
 class Solution {
     func buildArray(_ nums: [Int]) -> [Int] {
-        var ans: [Int] = []
-        for i in nums {
-            ans.append(nums[i] ?? 0)
-        }
-        return ans
+        return nums.map { nums[$0] }
     }
 }
